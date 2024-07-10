467ff230451e1a94eedbe2248688f3a49f8ca728
Darryl-win <logossoudarryl20@gmail.com>
2024-07-10 01:29:51 +0000

    intégration de firebase au projet

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:minimal_chat/firebase_options.dart';
 import 'package:minimal_chat/themes/light_mode.dart';
 
 import 'auth/login_or_register.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -11,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginOrRegister(),
       theme: lightMode,
