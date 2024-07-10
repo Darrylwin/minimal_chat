037eb8e557d91d8bbd7129b812c65cdc727581ee
Darryl-win <logossoudarryl20@gmail.com>
2024-07-10 11:40:08 +0000

    sign out method

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -19,6 +19,9 @@ class AuthService {
 // sign up
 
 //sign out
+Future<void> signOut() async {
+    return await _auth.signOut();
+}
 
 //errors
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../auth/auth_service.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  void logout() {
+    //get auth service
+    final _auth = AuthService();
+    _auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        actions: [
+          // logout button
+          IconButton(
+            onPressed: logout,
+            icon: Icon(Icons.logout),
+          )
+        ],
       ),
     );
   }
