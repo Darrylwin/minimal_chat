755d82e73711a61d3fca8aa1ff1e58c687beeb6d
Darryl-win <logossoudarryl20@gmail.com>
2024-07-12 01:05:47 +0000

    Theme sombre

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
     colorScheme: ColorScheme.light(
-      surface: Colors.grey.shade300,
-      primary: Colors.grey.shade500,
-      secondary: Colors.grey.shade200,
-      inversePrimary: Colors.grey.shade900,
-    )
-);
+  surface: Colors.grey.shade300,
+  primary: Colors.grey.shade500,
+  secondary: Colors.grey.shade200,
+  tertiary: Colors.white,
+  inversePrimary: Colors.grey.shade900,
+));
