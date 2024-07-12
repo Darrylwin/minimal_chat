d305736af6cbd1ce87c3e27c07a9bfa1966edaf9
Darryl-win <logossoudarryl20@gmail.com>
2024-07-12 02:42:00 +0000

    bubble colors with toggle themes

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_chat/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class ChatBubble extends StatelessWidget {
   final String message;
@@ -12,16 +14,26 @@ class ChatBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // correct colors for light vs dark mode
+    bool isDarkMode =
+        Provider.of<ThemeProvider>(context, listen: false).isDarkMode;
+
     return Container(
       decoration: BoxDecoration(
-        color: isCurrentUser ? Colors.green : Colors.grey.shade500,
+        color: isCurrentUser
+            ? (isDarkMode ? Colors.green.shade900 : Colors.green)
+            : (isDarkMode ? Colors.grey.shade900 : Colors.grey.shade300),
         borderRadius: BorderRadius.circular(12),
       ),
       padding: const EdgeInsets.all(16),
       margin: const EdgeInsets.symmetric(vertical: 2.5, horizontal: 25.0),
       child: Text(
         message,
-        style: TextStyle(color: Colors.white),
+        style: TextStyle(
+          color: isCurrentUser
+              ? Colors.white
+              : (isDarkMode ? Colors.white : Colors.black),
+        ),
       ),
     );
   }
