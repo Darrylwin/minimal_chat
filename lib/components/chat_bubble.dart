ab3302f2ea8991847c2f0ababa9639d7ce83bf25
Darryl-win <logossoudarryl20@gmail.com>
2024-07-12 00:21:10 +0000

    metode ChatBubble

---
+++ lib/components/chat_bubble.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  final String message;
+  final bool isCurrentUser;
+
+  const ChatBubble({
+    super.key,
+    required this.message,
+    required this.isCurrentUser,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: isCurrentUser ? Colors.green : Colors.grey.shade500,
+      ),
+      child: Text(message),
+    );
+  }
+}
