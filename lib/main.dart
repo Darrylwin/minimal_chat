af37146e24e3e2309df31cc2c2112cbf3068f2cc
Darryl-win <logossoudarryl20@gmail.com>
2024-07-12 01:25:06 +0000

    utilisation de la metode toggleTheme

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:minimal_chat/firebase_options.dart';
 import 'package:minimal_chat/services/auth/auth_gate.dart';
 import 'package:minimal_chat/themes/light_mode.dart';
+import 'package:minimal_chat/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const AuthGate(),
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
