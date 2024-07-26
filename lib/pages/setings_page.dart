34f7087c2d1df3fdc58f1a05f03fa7905a65ab46
Darryl-win <logossoudarryl20@gmail.com>
2024-07-26 00:16:24 +0000

    restructuration de la settings page

--- lib/pages/setings_page.dart
+++ lib/pages/setings_page.dart
@@ -9,42 +9,88 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
-        title: const Text(
-          "Settings",
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          title: const Text("Settings"),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.grey,
+          elevation: 0,
+          actions: [],
         ),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.grey,
-        elevation: 0,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(
-            30,
-          ),
-        ),
-        margin: const EdgeInsets.all(25),
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // dark mode
-            const Text('  Dark Mode'),
+        body:
+            // Container(
+            //   decoration: BoxDecoration(
+            //     color: Theme.of(context).colorScheme.secondary,
+            //     borderRadius: BorderRadius.circular(
+            //       30,
+            //     ),
+            //   ),
+            //   margin: const EdgeInsets.all(25),
+            //   padding: const EdgeInsets.all(16),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       // dark mode
+            //       Text(
+            //         '  Dark Mode',
+            //         style: TextStyle(
+            //             fontWeight: FontWeight.bold,
+            //             color: Theme.of(context).colorScheme.inversePrimary),
+            //       ),
 
-            // switch toggle
-            CupertinoSwitch(
-              value:
-                  Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
-              onChanged: (value) =>
-                  Provider.of<ThemeProvider>(context, listen: false)
-                    ..toggleTheme(),
+            //       // switch toggle
+            //       CupertinoSwitch(
+            //         value:
+            //             Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
+            //         onChanged: (value) =>
+            //             Provider.of<ThemeProvider>(context, listen: false)
+            //               ..toggleTheme(),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+
+            Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  margin: const EdgeInsets.only(left: 25, top: 10, right: 25),
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 25, top: 20, bottom: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // dark mode
+                      Text(
+                        '  Dark Mode',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+
+                      // switch toggle
+                      CupertinoSwitch(
+                        value:
+                            Provider.of<ThemeProvider>(context, listen: false)
+                                .isDarkMode,
+                        onChanged: (value) =>
+                            Provider.of<ThemeProvider>(context, listen: false)
+                              ..toggleTheme(),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
